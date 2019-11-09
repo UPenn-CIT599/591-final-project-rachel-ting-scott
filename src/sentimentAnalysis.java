2852988a3bdfff644e99bf2cc0d34ad801313cf3
sherry <tinglee@seas.upenn.edu>
2019-11-09 14:10:03 +0800

    added 'sentimentDict' instance variable to 'sentimentAnalysis' class

--- src/sentimentAnalysis.java
+++ src/sentimentAnalysis.java
@@ -1,9 +1,51 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class sentimentAnalysis {
+	private HashMap<String, String> sentimentDict;
+		
+	public sentimentAnalysis() {
+		sentimentDict = new HashMap<String, String>();
+		File negative = new File("negative-words.txt");
+		try {
+			Scanner in = new Scanner(negative);
+			while(in.hasNextLine()) {
+				String word = in.nextLine();
+				sentimentDict.put(word, "n");
+			}
+			in.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		File positive = new File("positive-words.txt");
+		try {
+			Scanner in2 = new Scanner(positive);
+			while(in2.hasNextLine()) {
+				String word = in2.nextLine();
+				sentimentDict.put(word, "p");
+			}
+			in2.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public HashMap<String, String> getSentimentDict() {
+		return sentimentDict;
+	}
+
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		sentimentAnalysis sA = new sentimentAnalysis();
+		for(String key : sA.getSentimentDict().keySet()) {
+			System.out.println(key + "," + sA.getSentimentDict().get(key));
+		}
 	}
 
 }
