762bec609dc1e6930b16e94a8fcdeee4f06e9828
sherry <tinglee@seas.upenn.edu>
2019-11-10 11:55:12 +0800

    updated sentimentAnalysis.java by adding three methods: createDictionary, wordCounter, scoreCounter

--- src/sentimentAnalysis.java
+++ src/sentimentAnalysis.java
@@ -5,9 +5,21 @@ import java.util.Scanner;
 
 public class sentimentAnalysis {
 	private HashMap<String, String> sentimentDict;
+	private int negativeWordCount;
+	private int positiveWordCount;
+	private int scoreOutput;
 		
 	public sentimentAnalysis() {
 		sentimentDict = new HashMap<String, String>();
+		negativeWordCount = 0;
+		positiveWordCount = 0;
+		scoreOutput = 0;
+	}
+		
+	/**
+	 * This method creates a dictionary of positive and negative words
+	 */
+	public void createDictionary() {
 		File negative = new File("negative-words.txt");
 		try {
 			Scanner in = new Scanner(negative);
@@ -36,10 +48,52 @@ public class sentimentAnalysis {
 	}
 	
 	
+	/**
+	 * This method counts the positive and negative words in the text
+	 * @param NLPAnalysisResult HashMap
+	 */
+	public void wordCounter(HashMap<String, Integer> NLPAnalysisResult) {
+		for (String key : NLPAnalysisResult.keySet()) {
+			String sentimentResult = sentimentDict.get(key.toLowerCase());
+			if (sentimentResult.equals("n")) {
+				negativeWordCount += NLPAnalysisResult.get(key);
+			}
+			else if (sentimentResult.equals("p")) {
+				positiveWordCount += NLPAnalysisResult.get(key);
+			}
+		}
+	}
+	
+	/**
+	 * This method calculates the positivity score by assigning the negative words with -1
+	 * and the positive words with 1.
+	 * @return scoreOutput
+	 */
+	public int scoreCounter() {		
+		scoreOutput = negativeWordCount * (-1) + positiveWordCount;				
+		return scoreOutput;
+	}
+	
+	
 	public HashMap<String, String> getSentimentDict() {
 		return sentimentDict;
 	}
 
+
+	public int getNegativeWordCount() {
+		return negativeWordCount;
+	}
+
+
+	public int getPositiveWordCount() {
+		return positiveWordCount;
+	}
+
+
+	public int getScoreOutput() {
+		return scoreOutput;
+	}
+	
 
 //	public static void main(String[] args) {
 //		sentimentAnalysis sA = new sentimentAnalysis();
