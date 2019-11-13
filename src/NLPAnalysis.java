3f634c5139b378fd07de6ef9752a45f1fb5f6e6e
rushi650 <friend.rachel@gmail.com>
2019-11-13 10:30:18 -0800

    modified NLPAnalysis and KeywordAnalysis classes

--- src/NLPAnalysis.java
+++ src/NLPAnalysis.java
@@ -1,10 +1,15 @@
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 
 import opennlp.tools.postag.POSModel;
 import opennlp.tools.postag.POSTaggerME;
+import opennlp.tools.tokenize.SimpleTokenizer;
 import opennlp.tools.tokenize.Tokenizer;
 import opennlp.tools.tokenize.TokenizerME;
 import opennlp.tools.tokenize.TokenizerModel;
@@ -17,11 +22,17 @@ import opennlp.tools.tokenize.TokenizerModel;
  */
 public class NLPAnalysis {
 	//	private String[] userWordsArray;
-	private String userWords;
+	private String userWords = "The story goes like this. John is a 26 year-old. He is older than me, but I am smarter than him. "
+			+ "sad sadly sadder saddest slow slowly slower slowest not happy the news my car there are no cars. "
+			+ "she doesn't want to because she does not want to; however, she should. Whose car is that? "
+			+ "I want the teacher who is nice.";
+	private String userWordsMinusStopWords;
 	private InputStream tokenModelIn = null;
 	private InputStream posModelIn = null;
 	private InputStream dictLemmatizer = null;
 
+	//customized list of stop words
+	private String[] stopWords = {"the", "and", "an", "a", "or", "with"};
 	private String[] tokensArray;
 	private String[] tagsArray;
 	private String[] lemmaArray;
@@ -36,6 +47,32 @@ public class NLPAnalysis {
 	public NLPAnalysis(String userWords) {
 		this.userWords = userWords;
 	}
+	/**
+	 * Overloaded constructor
+	 */
+	public NLPAnalysis() {
+		
+	}
+
+	/**
+	 * Remove stop words from the user's URL text. 
+	 * @param userWords is the text input
+	 * @param stopWords are the words that are custom defined as stop words 
+	 */
+	public String removeStopWords (String userWords, String[] stopWords) {
+		System.out.println("Original text: " + userWords);
+		userWords = userWords.toLowerCase().trim();
+
+		ArrayList<String> wordList = new ArrayList<>();
+		wordList.addAll(Arrays.asList(userWords.split(" ")));
+
+		List<String> stopWordList = new ArrayList<>();
+		stopWordList.addAll(Arrays.asList(stopWords));
+
+		wordList.removeAll(stopWordList);
+		System.out.println("Text without stop words: " + wordList.toString());
+		return userWordsMinusStopWords;
+	}
 
 	/**
 	 * Helper method that takes the String of user's text and tokenizes them
@@ -46,15 +83,16 @@ public class NLPAnalysis {
 			tokenModelIn = new FileInputStream("en-token.bin");
 			TokenizerModel tokenModel = new TokenizerModel(tokenModelIn);
 			Tokenizer tokenizer = new TokenizerME(tokenModel);
+
 			tokensArray = tokenizer.tokenize(userWords.toLowerCase());
 			//TESTING
-//			System.out.println("tokensArray:");
-//			for (String str : tokensArray) {
-//				System.out.print(str + "\t");
-//			}
-//			System.out.println(); //TESTING
-//			System.out.println(tokensArray[3]); //TESTING
-		
+			//			System.out.println("tokensArray:");
+			//			for (String str : tokensArray) {
+			//				System.out.print(str + "\t");
+			//			}
+			//			System.out.println(); //TESTING
+			//			System.out.println(tokensArray[3]); //TESTING
+
 		} catch (IOException e) {
 			// Model loading failed, handle the error
 			e.printStackTrace();
@@ -90,7 +128,54 @@ public class NLPAnalysis {
 		System.out.println(tokenToCountMap);
 		return tokenToCountMap;
 	}
-	
+
+	public String[] tagger() {
+		String[] temp = tokenizer();
+//		SimpleTokenizer tokenizer = SimpleTokenizer.INSTANCE; //TESTING
+//		String[] tokens = tokenizer.tokenize("John has a sister named Penny."); //TESTING
+		// reading parts-of-speech model to a stream
+		try {
+			posModelIn = new FileInputStream("en-pos-maxent.bin");
+			// loading the parts-of-speech model from stream
+			POSModel posModel = new POSModel(posModelIn);
+			// initializing the parts-of-speech tagger with model
+			POSTaggerME posTagger = new POSTaggerME(posModel);
+			// Tagger tagging the tokens
+			tagsArray = posTagger.tag(temp);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+
+		
+		
+//		SimpleTokenizer tokenizer = SimpleTokenizer.INSTANCE;
+//		String[] tokens = tokenizer.tokenize("John has a sister named Penny.");
+//
+//		InputStream inputStreamPOSTagger = getClass()
+//				.getResourceAsStream("en-pos-maxent.bin");
+//		POSModel posModel;
+//		try {
+//			posModel = new POSModel(inputStreamPOSTagger);
+//			POSTaggerME posTagger = new POSTaggerME(posModel);
+//			tagsArray = posTagger.tag(tokens);
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+		
+		//TESTING
+		System.out.print("Tags from .tagger: ");
+		for (String str : tagsArray) {
+			System.out.print(str +  "\t");
+		}
+		return tagsArray;
+
+	}
 	/**
 	 * Tags the parts of speech of each word in the user's input and stores those key-value pairs in a hashmap
 	 * @return HashMap of token to POS
@@ -107,7 +192,7 @@ public class NLPAnalysis {
 			POSTaggerME posTagger = new POSTaggerME(posModel);
 			// Tagger tagging the tokens
 			tagsArray = posTagger.tag(temp);
-//			String tags[] = posTagger.tag(tokensArray); //look up tags as datatype
+			//			String tags[] = posTagger.tag(tokensArray); //look up tags as datatype
 		}
 		catch (IOException e) {
 			// Model loading failed, handle the error
@@ -145,7 +230,7 @@ public class NLPAnalysis {
 		System.out.println(tokenToPOSTagMap);
 		return tokenToPOSTagMap;
 	}
-	
+
 	/**
 	 * Lematizes the words from the user's URL (minus stop words)
 	 * -----this might need to move to the KeywordAnalysis class-----
@@ -173,12 +258,20 @@ public class NLPAnalysis {
 
 	//main for TESTING
 	public static void main(String[] args) {
-		NLPAnalysis nlp = new NLPAnalysis("John is 26 years old. He is older than me, but I am smarter than him. "
-				+ "sad sadly sadder saddest slow slowly slower slowest not happy the news my car there are no cars. "
-				+ "she doesn't want to because she does not want to; however, she should. Whose car is that? "
-				+ "I want the teacher who is nice.");
-
+//		NLPAnalysis nlp = new NLPAnalysis("The story goes like this. John is a 26 year-old. He is older than me, but I am smarter than him. "
+//				+ "sad sadly sadder saddest slow slowly slower slowest not happy the news my car there are no cars. "
+//				+ "she doesn't want to because she does not want to; however, she should. Whose car is that? "
+//				+ "I want the teacher who is nice.");
+NLPAnalysis nlp = new NLPAnalysis();
+		System.out.println("userWords: " + nlp.userWords);
 		nlp.createTokenToCountMap();
-//		nlp.createTokenToPOSTagMap();
+
+		//		removeStopWords(sentence, stopWords);
+		nlp.removeStopWords(nlp.userWords, nlp.stopWords);
+		//		nlp.removeStopWords(nlp.userWords, stopWords);
+
+		nlp.tagger();
+
+		//		nlp.createTokenToPOSTagMap();
 	}
 }
