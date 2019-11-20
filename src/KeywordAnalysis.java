c8ed62ef79f2b2b04e1fdab81268990fdbad7788
rachel-friend <friend.rachel@gmail.com>
2019-11-20 10:11:36 -0800

    deleted pom.xml since we don't need to use Maven and deleted location and person extraction models since we aren't using them

--- src/KeywordAnalysis.java
+++ src/KeywordAnalysis.java
@@ -1,11 +1,32 @@
+import java.io.File;
 import java.io.InputStream;
 import java.util.ArrayList;
+
+import opennlp.tools.ml.maxent.io.GISModelWriter;
+import opennlp.tools.ml.model.DataIndexer;
+import opennlp.tools.ml.model.MaxentModel;
 
 public class KeywordAnalysis {	
 	private InputStream keywordModelIn = null;
 	private String[] keywordArray;
 	//or
 	private ArrayList<String> keywordArrayList;
+	
+	/**
+	 * 
+	 */
+	public static MaxentModel trainModel(DataIndexer di, int iterations) {
+		
+	}
+	public void OnePassDataIndexer(EventStream events) {
+		
+	}
+	File outputFile = new File(modelFileName+".bin.gz");
+	GISModelWriter writer = new SuffixSensitiveGISModelWriter(model, outputFile);
+	writer.persist();
+	
+	//load the model
+	GISModel m = new SuffixSensitiveGISModelReader(new File(modelFileName)).getModel();
 	
 	/**
 	 * Use our trained model to find keywords and thus identify the topic(s) of the text
---
+++ src/Summarizer.java
@@ -0,0 +1,9 @@
+
+public class Summarizer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
