6e718a5626010b8e99b5848c1574b35cfdf4e61a
sherry <tinglee@seas.upenn.edu>
2019-11-29 13:33:21 +0800

    added GUI to the RUNNER class

--- src/RUNNER.java
+++ src/RUNNER.java
@@ -1,10 +1,24 @@
+import javax.swing.JFrame;
+
 /**
  * This class runs the output for the keyword extraction and sentiment analysis of a user's URL input
  * @author Rachel Friend, Scott Theer, Ting-Hsuan Lee
  *
  */
-public class RUNNER {
-
+public class RUNNER implements Runnable{
+	@Override
+	public void run() {
+	//create all the components	
+	JFrame frame = new JFrame("Website Analyzer");
+	
+	
+		
+		
+	//required pieces of code
+		
+		
+	}
+	
 	public static void main(String[] args) {
 		NLPData nlp = new NLPData();
 
