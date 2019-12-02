4525466ae9b1c8cf92da2ff3b7c7feeff4c4bdc1
rachel-friend <friend.rachel@gmail.com>
2019-12-02 15:55:59 -0800

    adding nlp methods tests

---
+++ src/NLPDataTest.java
@@ -0,0 +1,42 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class NLPDataTest {
+
+	@Test
+	void testTokenizer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCreateTokenToCountMap() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testLemmatizer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindTopLemma() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindPeople() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindTopPeople() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSentenceDetector() {
+		fail("Not yet implemented");
+	}
+
+}
